f4676c2b72806713425d066cf27db47676a1068a
Cameron Richards <cameron.richards0@icloud.com>
2016-05-26 19:22:43 +0100

    Spacedoor not quite working. Stencil is. Needs to open/kill collider.

---
+++ Assets/Scripts/DoorPlate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorPlate : Interactable {
+
+    bool move = false;
+    Quaternion oldRot;
+    Quaternion newRot;
+
+    public Quaternion relative;
+    void Update()
+    {
+        if(move)
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, newRot, 0.3f);
+            relative = Quaternion.Inverse(transform.localRotation) * newRot;
+
+            if(relative.w == 1)
+            {
+                move = false;
+            }
+        }
+    }
+
+    public override void InteractTrigger()
+    {
+        base.InteractTrigger();
+        move = true;
+        oldRot = transform.localRotation;
+        newRot = transform.localRotation * Quaternion.Euler(0, 0, 45);
+    }
+}
--- Assets/Scripts/MountainDoor.cs
+++ Assets/Scripts/MountainDoor.cs
@@ -3,28 +3,46 @@ using System.Collections;
 
 public class MountainDoor : MonoBehaviour {
 
-    Interactable[] all;
-    Interactable one;
-    Interactable two;
-    Interactable three;
+    DoorPlate[] all;
+    DoorPlate one;
+    DoorPlate two;
+    DoorPlate three;
+
+    bool open;
 
     void Awake()
     {
-        all = GetComponentsInChildren<Interactable>();
+        all = GetComponentsInChildren<DoorPlate>();
+        one = all[0];
+        two = all[1];
+        three = all[2];
     }
 
 	// Use this for initialization
 	void Start () {
-	foreach (Interactable ring in all)
+        foreach (DoorPlate ring in all)
         {
             int rot = Random.Range(1, 8) * 45;
-            print (rot);
-            ring.gameObject.transform.localRotation *= Quaternion.Euler(0, 0, rot);
+            ring.gameObject.transform.localRotation *= Quaternion.Euler(0, 0, rot); 
+        }
+
+        if(open)
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(-90, 0, -90), 0.3f);
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	if(one.relative == two.relative && two.relative == three.relative)
+        {
+            Open();
+        }
 	}
+
+    void Open()
+    {
+        print(true);
+        open = true;
+    }
 }
